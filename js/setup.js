375381d3192e3ffa6c7438adb632f3db14d5918a
Evgeny Vorona <zarin417@gmail.com>
2020-10-29 13:26:48 +0200

    Реализует открытие/закрытие окна настроек, выбор цвета элементов персонажа и валидацию поля имени

--- js/setup.js
+++ js/setup.js
@@ -35,11 +35,28 @@ const EYES_COLORS = [
   `yellow`,
   `green`
 ];
+const FIREBALL_COLORS = [
+  `#ee4830`,
+  `#30a8ee`,
+  `#5ce6c0`,
+  `#e848d5`,
+  `#e6e848`
+];
 const WIZARDS_AMOUNT = 4;
 const setupWindow = document.querySelector(`.setup`);
 const setupSimilar = setupWindow.querySelector(`.setup-similar`);
+const setupOpen = document.querySelector(`.setup-open`);
+const setupClose = setupWindow.querySelector(`.setup-close`);
+const setupUserName = setupWindow.querySelector(`.setup-user-name`);
+const setupPlayer = setupWindow.querySelector(`.setup-player`);
+const wizardCoat = setupPlayer.querySelector(`.wizard-coat`);
+const wizardCoatInput = setupPlayer.querySelector(`input[name='coat-color']`);
+const wizardEyes = setupPlayer.querySelector(`.wizard-eyes`);
+const wizardEyesInput = setupPlayer.querySelector(`input[name='eyes-color']`);
+const fireballWrap = setupPlayer.querySelector(`.setup-fireball-wrap`);
+const fireball = fireballWrap.querySelector(`.setup-fireball`);
+const fireballInput = fireballWrap.querySelector(`input[name='fireball-color']`);
 
-setupWindow.classList.remove(`hidden`);
 setupSimilar.classList.remove(`hidden`);
 
 // Get a random number in a given range
@@ -95,3 +112,85 @@ const insertFragment = () => {
 };
 
 insertFragment();
+
+/* Create listeners for buttons to open and close the settings window; for changing wizard's coat, eyes and fireball colors.
+  Change validation massage for user name field.
+*/
+const setupUserNameValidationHandler = () => {
+  if (setupUserName.validity.tooShort) {
+    setupUserName.setCustomValidity(`Имя не может быть меньше 2-х символов`);
+  } else if (setupUserName.validity.valueMissing) {
+    setupUserName.setCustomValidity(`Поле обязательно для заполнения`);
+  } else {
+    setupUserName.setCustomValidity(``);
+  }
+};
+
+const setupWindowEscPressHandler = (evt) => {
+  if (evt.key === `Escape` && document.activeElement !== setupUserName) {
+    evt.preventDefault();
+    setupWindow.classList.add(`hidden`);
+  }
+};
+
+const getWizardPartColor = (colors) => {
+  const color = colors[getRandomInteger(0, colors.length - 1)];
+  return color;
+};
+
+const setWizardPartColorFill = (input, element, colors) => {
+  input.value = getWizardPartColor(colors);
+  element.style.fill = input.value;
+};
+
+const setWizardPartColorBg = (input, element, colors) => {
+  input.value = getWizardPartColor(colors);
+  element.style.backgroundColor = input.value;
+};
+
+const wizardCoatFillHandler = () => {
+  setWizardPartColorFill(wizardCoatInput, wizardCoat, COAT_COLORS);
+};
+
+const wizardEyesFillHandler = () => {
+  setWizardPartColorFill(wizardEyesInput, wizardEyes, EYES_COLORS);
+};
+
+const wizardFireballBgHandler = () => {
+  setWizardPartColorBg(fireballInput, fireballWrap, FIREBALL_COLORS);
+};
+
+const openSetupWindow = () => {
+  setupWindow.classList.remove(`hidden`);
+  document.addEventListener(`keydown`, setupWindowEscPressHandler);
+  setupUserName.addEventListener(`invalid`, setupUserNameValidationHandler);
+  wizardCoat.addEventListener(`click`, wizardCoatFillHandler);
+  wizardEyes.addEventListener(`click`, wizardEyesFillHandler);
+  fireball.addEventListener(`click`, wizardFireballBgHandler);
+};
+
+const closeSetupWindow = () => {
+  setupWindow.classList.add(`hidden`);
+  document.removeEventListener(`keydown`, setupWindowEscPressHandler);
+  setupUserName.removeEventListener(`invalid`, setupUserNameValidationHandler);
+  wizardCoat.removeEventListener(`click`, wizardCoatFillHandler);
+  wizardEyes.removeEventListener(`click`, wizardEyesFillHandler);
+  fireball.removeEventListener(`click`, wizardFireballBgHandler);
+};
+
+// Add listeners
+setupOpen.addEventListener(`click`, openSetupWindow);
+
+setupOpen.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    openSetupWindow();
+  }
+});
+
+setupClose.addEventListener(`click`, closeSetupWindow);
+
+setupClose.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    closeSetupWindow();
+  }
+});
